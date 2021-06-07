f346a7686b5ad9b7191d5f6eae38e64063c7dddd
James Wyn <thisisjameswyn@gmail.com>
2021-06-07 07:30:00 -0700

    add a new field to machine

---
+++ Factory/Migrations/20210607142709_nownow.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class nownow : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MyInt",
+                table: "Machines");
+
+            migrationBuilder.DropColumn(
+                name: "Number",
+                table: "Machines");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Production",
+                table: "Machines",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Production",
+                table: "Machines");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MyInt",
+                table: "Machines",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Number",
+                table: "Machines",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -9,16 +9,18 @@ namespace Factory.Models
       this.JoinEntities = new HashSet<EngineerMachine>();
     }
     public int MachineId { get; set; }
+
     public string Name { get; set; }
-    public int Number { get; set; }
-    public int MyInt { get; set; }
+    public string Production { get; set; }
+    // public int Number { get; set; }
+    // public int MyInt { get; set; }
     public virtual ICollection<EngineerMachine> JoinEntities { get; set; }
-    public string NameNumber
-    {
-      get
-      {
-        return Name + Number;
-      }
-    }
+    // public string NameNumber
+    // {
+    //   get
+    //   {
+    //     return Name + Number;
+    //   }
+    // }
   }
 }
